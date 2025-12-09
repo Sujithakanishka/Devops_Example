3eb6c8ee52acdb4ff4045c615370f0856899e04f
Sujithakanishka <113288188+Sujithakanishka@users.noreply.github.com>
2025-12-09 17:10:28 +0530

    Change server port from 3000 to 8080

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
   res.send("Hello! Your Node.js project is running successfully on AWS EC2!");
 });
 
-const port = 3000;
+const port = 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
